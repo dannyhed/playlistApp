6b37b7666104a81ce8dbf3db3c6283daa50ddebc
koyyo <dkamen74@gmail.com>
2024-03-01 16:32:15 -0700

    finished2

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Portfolio, Project
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Portfolio)
+admin.site.register(Project)
+
--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
-from django.contrib.auth import view as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 #path function defines a url pattern
@@ -8,7 +9,12 @@ urlpatterns = [
 # views.index is the function defined in views.py
 # name='index' parameter is to dynamically create url
 # example in html <a href="{% url 'index' %}">Home</a>.
+        
+    path('admin/', admin.site.urls),
+    #connect path to portfolio_app urls
+    #path('', include('portfolio_app.urls')),
     path('', views.index, name='index'),
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
+
